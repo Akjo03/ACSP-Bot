1467492355ffb841c12445060bc3bb4467bef4c7
Akjo03 <lukas.kueffer@outlook.com>
2023-03-06 16:16:50 +0100

    Started beginning on help command and added some messsages to the bot config

--- src/main/java/io/github/akjo03/discord/cscbot/commands/HelpCommand.java
+++ src/main/java/io/github/akjo03/discord/cscbot/commands/HelpCommand.java
@@ -1,5 +1,6 @@
 package io.github.akjo03.discord.cscbot.commands;
 
+import io.github.akjo03.discord.cscbot.constants.CscCommandArgumentTypes;
 import io.github.akjo03.discord.cscbot.util.command.CommandInitializer;
 import io.github.akjo03.discord.cscbot.util.command.CscCommand;
 import io.github.akjo03.discord.cscbot.util.command.argument.CscCommandArguments;
@@ -27,6 +28,27 @@ public class HelpCommand extends CscCommand {
 	public void execute(@NotNull MessageReceivedEvent event, @NotNull CscCommandArguments arguments) {
 		logger.info("Executing help command...");
 
+		String subcommand = arguments.getSubcommand();
+		if (subcommand == null) {
+			logger.error("Subcommand is null!");
+			return;
+		}
+
+		switch (subcommand) {
+			case "command" -> onCommandHelp(event, arguments);
+			default -> logger.error("Unknown subcommand: " + subcommand);
+		}
+
 		logger.success("Command help successfully executed!");
+	}
+
+	public void onCommandHelp(@NotNull MessageReceivedEvent event, @NotNull CscCommandArguments arguments) {
+		String commandName = arguments.getSubcommandArgument("command", CscCommandArgumentTypes.STRING);
+		if (commandName == null) {
+			logger.error("Command name is null!");
+			return;
+		}
+
+
 	}
 }
